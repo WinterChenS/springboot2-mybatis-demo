c4fb1f61867e08ea222f91473d90542c2ba30b94
WinterChenS <1085143002@qq.com>
2018-04-20 21:19:54 +0800

    修复无法分页的bug

--- src/main/java/com/winterchen/config/DataSourceConfig.java
+++ src/main/java/com/winterchen/config/DataSourceConfig.java
@@ -1,5 +1,8 @@
 package com.winterchen.config;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInterceptor;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
@@ -32,11 +35,15 @@ public class DataSourceConfig {
     @Autowired
     private Environment env;
 
+    @Autowired
+    private PageInterceptor pageInterceptor;
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean fb = new SqlSessionFactoryBean();
         fb.setDataSource(dataSource);
+        //该配置非常的重要，如果不将PageInterceptor设置到SqlSessionFactoryBean中，导致分页失效
+        fb.setPlugins(new Interceptor[]{pageInterceptor});
         fb.setTypeAliasesPackage(env.getProperty("mybatis.type-aliases-package"));
         fb.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(env.getProperty("mybatis.mapper-locations")));
         return fb.getObject();
